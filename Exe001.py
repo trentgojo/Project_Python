b86a802dd0baa2c3bd299663bd2b378fd3746c4b
trentgojo <trentgojo@gmail.com>
2023-05-30 18:16:55 -0300

    Atualização_30_05

---
+++ Exe001.py
@@ -0,0 +1,14 @@
+nome=input("Digite um funcionário: ")
+empresa=input("Digite a instituição: ")
+qtdefuncionarios=int(input("Digite a qtde de funcionários: "))
+mediaMensalidade=float(input("Digite a média da mensalidade: "))
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtdefuncionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
+print("==============Verifique os tipos de dados abaixo:==============")
+print("O tipo de dado da variavel [nome] é: ",type(nome))
+print("O tipo de dado da variavel [empresa] é: ",type(empresa))
+print("O tipo de dado da variavel [qtdefuncionarios] é: ",type(qtdefuncionarios))
+print("O tipo de dado da variavel [mediaMensalidade] é: ",type(mediaMensalidade))
+
+
