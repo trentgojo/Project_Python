b54b06425eec1a6cdba834a260b271cd8833174c
trentgojo <trentgojo@gmail.com>
2023-06-01 18:12:43 -0300

    Update_01-06

---
+++ Exe009.py
@@ -0,0 +1,6 @@
+nome=input("Digite o nome: ")
+idade=int(input("Digite a idade: "))
+prioridade="NAO"
+if idade>=65:
+    prioridade="SIM"
+print("O paciente " + nome + " possui atendimento prioritário? " + prioridade)
