38979e321c4ed35c36dda3127561a54c4f4cf797
trentgojo <trentgojo@gmail.com>
2023-06-14 18:09:04 -0300

    UPDATE_14/06

---
+++ Exe011.py
@@ -0,0 +1,17 @@
+print("Teste de Nomes: ")
+
+nome = "Thiago"
+sobrenome_mae = "Carvalho"
+sobrenome_pai = "Porto"
+iniciais = nome[0] + sobrenome_mae[0] + sobrenome_pai[0]
+
+print(nome)
+print(sobrenome_mae)
+print(sobrenome_pai)
+print(iniciais)
+
+print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais)
+
+print(nome+" "+sobrenome_mae+" "+sobrenome_pai)
+
+print("Fim")
