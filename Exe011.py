5a767005ce58cee20a9a5322ac90b2cd941ab2c5
trentgojo <trentgojo@gmail.com>
2023-06-14 21:18:22 -0300

    Update

--- Exe011.py
+++ Exe011.py
@@ -1,4 +1,4 @@
-print(">>>Teste de Nomes ")
+print(">>>Teste de Nomes<<< ")
 
 nome = "Thiago"
 sobrenome_mae = "Carvalho"
@@ -6,7 +6,7 @@ sobrenome_pai = "Porto"
 iniciais = nome[0] + sobrenome_mae[0] + sobrenome_pai[0]
 empresa = "ikross"
 
-print("Informações: ")
+print("<<<Informações: ")
 print("Nome do usuário:",nome, sobrenome_pai, sobrenome_mae)
 print("Quais são suas iniciais:",iniciais)
 print("Nome da empresa:",empresa)
@@ -15,8 +15,9 @@ print("Nome da empresa:",empresa)
 
 print("<<<Resolução:>>>" )
 
-print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais+".")
-
 print(nome+" "+sobrenome_mae+" "+sobrenome_pai)
+print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais+".")
+print("Criando o Email: "+nome+"."+sobrenome_pai+"@"+empresa+".com" )
+
 
 print(">>>Fim")
