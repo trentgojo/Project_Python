5d3a33c2e707d0ed696adb6825cd90046735ff4a
trentgojo <trentgojo@gmail.com>
2023-06-15 16:54:24 -0300

    Update_Exe11

--- Exe011.py
+++ Exe011.py
@@ -1,4 +1,4 @@
-print(">>>Teste de Nomes<<< ")
+print(">>>Teste de Nomes!!!!!!! ")
 
 nome = "Thiago"
 sobrenome_mae = "Carvalho"
@@ -6,18 +6,23 @@ sobrenome_pai = "Porto"
 iniciais = nome[0] + sobrenome_mae[0] + sobrenome_pai[0]
 empresa = "ikross"
 
-print("<<<Informações: ")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("<<<Informações>>>")
 print("Nome do usuário:",nome, sobrenome_pai, sobrenome_mae)
-print("Quais são suas iniciais:",iniciais)
+print("Quais são minhas iniciais:",iniciais)
 print("Nome da empresa:",empresa)
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
-
-print("<<<Resolução:>>>" )
+print("<<<Resultado:>>>" )
 
 print(nome+" "+sobrenome_mae+" "+sobrenome_pai)
 print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais+".")
 print("Criando o Email: "+nome+"."+sobrenome_pai+"@"+empresa+".com" )
-
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print("Procurando letras especificas: ")
+print(nome[1],sobrenome_pai[4],sobrenome_mae[5])
+print(nome[4],sobrenome_mae[3],sobrenome_pai[3])
+print(sobrenome_pai[4],sobrenome_mae[2],nome[1])
 
 print(">>>Fim")
