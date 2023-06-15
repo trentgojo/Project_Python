aaef8ee38ae5148d3676e673a71cb654f657f6e9
trentgojo <trentgojo@gmail.com>
2023-06-14 21:10:23 -0300

    Update

--- Exe011.py
+++ Exe011.py
@@ -1,17 +1,22 @@
-print("Teste de Nomes: ")
+print(">>>Teste de Nomes ")
 
 nome = "Thiago"
 sobrenome_mae = "Carvalho"
 sobrenome_pai = "Porto"
 iniciais = nome[0] + sobrenome_mae[0] + sobrenome_pai[0]
+empresa = "ikross"
 
-print(nome)
-print(sobrenome_mae)
-print(sobrenome_pai)
-print(iniciais)
+print("Informações: ")
+print("Nome do usuário:",nome, sobrenome_pai, sobrenome_mae)
+print("Quais são suas iniciais:",iniciais)
+print("Nome da empresa:",empresa)
 
-print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais)
+
+
+print("<<<Resolução:>>>" )
+
+print("Meu nome completo é " +nome+ " " +sobrenome_mae + " " +sobrenome_pai + " e minhas iniciais são"+ " " +iniciais+".")
 
 print(nome+" "+sobrenome_mae+" "+sobrenome_pai)
 
-print("Fim")
+print(">>>Fim")
