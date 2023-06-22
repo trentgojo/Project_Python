428e5a9a337e38c9f7c21c6ac13f82b849ebac05
trentgojo <trentgojo@gmail.com>
2023-06-22 17:37:59 -0300

    Updade

---
+++ Exe012.py
@@ -0,0 +1,17 @@
+idade = 18
+type(idade)
+print("Maior ou Menor")
+if idade >= 18:
+    print("Acesso liberado")
+else: print("Acesso negado")
+print("____________________________________")
+print("Raiz Quadrada")
+import math
+print("A raiz quadrada de 64 é:" ,math.sqrt(64))
+print("____________________________________")
+print("Fatorial")
+num1 = 2
+num2 = 5.5
+num5 = 20
+
+print(math.factorial(num1) + num5)
