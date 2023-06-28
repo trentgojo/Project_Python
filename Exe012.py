a7837ff883d2e5f83ab1ce24d514b2efaf36d1b8
Thiago Porto <portokross@gmail.com>
2023-06-28 00:07:08 -0300

    Update

--- Exe012.py
+++ Exe012.py
@@ -9,12 +9,19 @@ print("____________________________________")
 print("Raiz Quadrada")
 import math
 print("A raiz quadrada de 64 é:" ,math.sqrt(64))
+
 print("____________________________________")
 print("Fatorial")
 num1 = 2
 num2 = 5.5
 num5 = 20
+num4 = 35
+
+print("A soma dos nummeros num1 + num5: ")
 print(math.factorial(num1) + num5)
+print("A soma dos numero num5 + num4 é: ")
+print(math.factorial(num1) + num4/2)
+
 print("____________________________________")
 print("O meu primeiro programa em Python")
 num6 = 100
