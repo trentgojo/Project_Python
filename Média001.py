ae3574dbd70a53cd31598206e0b26f5a1c068c77
trentgojo <trentgojo@gmail.com>
2023-06-28 18:30:26 -0300

    Update

--- Média001.py
+++ Média001.py
@@ -5,3 +5,18 @@ peso1 = float(input("Digite o peso da primeira avaliação: "))
 peso2 = float(input("Digite o peso da segunda avaliação: "))
 AV1 = float(input("Digite a nota da primeira AV1: "))
 AV2 = float(input("Digite a nota da segunda AV2: "))
+
+media = ((peso1*AV1) + (peso2*AV2) / (peso1+peso2))
+print("---------------------------------------------------")
+print("A media do aluno é:" ,media)
+print("---------------------------------------------------")
+
+resultado = float(input("Média Final: "))
+if resultado >= 5.0:
+    print("A aluno obteve a nota acima de 5.0: APROVADO")
+else:
+    print("O aluno obteve a note abaixo de 5.0: REPROVADO")
+
+print("Boas Férias")
+print("---------------------------------------------------")
+input("Aperte enter para sair")
