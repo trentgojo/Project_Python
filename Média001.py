8eccf042dee0f755296f11f2f1382b489bb76c9e
trentgojo <trentgojo@gmail.com>
2023-06-28 17:54:03 -0300

    Update

---
+++ Média001.py
@@ -0,0 +1,7 @@
+print("Média do aluno")
+print("---------------------------------------------------")
+
+peso1 = float(input("Digite o peso da primeira avaliação: "))
+peso2 = float(input("Digite o peso da segunda avaliação: "))
+AV1 = float(input("Digite a nota da primeira AV1: "))
+AV2 = float(input("Digite a nota da segunda AV2: "))
