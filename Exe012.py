8eadd7ff8b3a7daa8de4e240b0ef060a8ca6a993
trentgojo <trentgojo@gmail.com>
2023-07-04 18:59:58 -0300

    Update

--- Exe012.py
+++ Exe012.py
@@ -1,33 +1,14 @@
-idade = 18
-type(idade)
-print("Maior ou Menor")
-print("Se o aluno for maior de 18: ")
-if idade >= 18:
-    print("Acesso liberado")
-else: print("Acesso negado")
-print("____________________________________")
-print("Raiz Quadrada")
-import math
-print("A raiz quadrada de 64 é:" ,math.sqrt(64))
+print("<<Maior ou Menor>>")
+print("-="*25)
 
-print("____________________________________")
-print("Fatorial")
-num1 = 2
-num2 = 5.5
-num5 = 20
-num4 = 35
+#IDADE USANDO O SINAL (< OU >)
 
-print("A soma dos nummeros num1 + num5: ")
-print(math.factorial(num1) + num5)
-print("A soma dos numero num5 + num4 é: ")
-print(math.factorial(num1) + num4/2)
+#ENTRADA DE DADOS
+idade = input("Digite a idade: ")
 
-print("____________________________________")
-print("O meu primeiro programa em Python")
-num6 = 100
-num7 = 350
-print("O resultado de",num6,"+",num7,"é:",num6+num7)
+if idade >= str(18):
+    print("Acesso Liberador")
+else: print("Acesso Negado")
+print("-="*25)
 
-
-print("____________________________________")
-print("Fim do programa")
+input("Clique para sair")
--- Exe013.py
+++ Exe013.py
@@ -1,5 +1,14 @@
-print("O Inicio")
+print("<<<O Inicio>>>")
+print("-="*30)
+
+#DADOS
 Num1 = int(input("Digite um numero: "))
-Num2 = int(input("Digite outro numero: "))
+Num2 = int(input("Digite o segundo numero: "))
+print("-="*30)
+
+#SOMA DOS NUMEROS
 print("O resultado de",Num1,"+",Num2, "é:",Num1+Num2)
 print("Fim do programa")
+print("-="*30)
+
+input("Clique para sair")
