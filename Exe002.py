c3607765b0cb9f71d9800e18e13b75677e4d9716
trentgojo <trentgojo@gmail.com>
2023-07-04 17:57:37 -0300

    update

--- Exe002.py
+++ Exe002.py
@@ -1,6 +1,15 @@
-responsavel=input("Digite o nome do responsável: ")
-funcionario=input("Digite o nome do funcionário: ")
-evento=input("Digite o nome do evento: ")
-valor=float(input("Digite o valor que será ressarcido: "))
+print("-=-=-=-=-=-=DECLARACAO DA EMPRESA-=-=-=-=-=-=")
 
-print("Declaro para o senhor " + responsavel + ", que o senhor" + funcionario + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor) + " com a entrada.")
+#DECLARAÇÃO DA EMPRESA
+responsavel=input("Digite o nome do responsavel: ")
+funcionario=input("Digite o nome do funcionario: ")
+evento=input("Digite o nome do evento: ")
+valor=float(input("Digite o valor que sera ressarcido: "))
+print("-="*30)
+
+#SOLUÇÃO
+print("Declaro para o senhor",responsavel,", que o senhor",funcionario,
+      "esteve presente no evento",evento,", e gastou o valor de R$",str(valor),"reais com a entrada.")
+print("-="*30)
+
+input("Clique no ENTER para sair")
